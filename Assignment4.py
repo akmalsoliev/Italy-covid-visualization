bb95ccf8d44529bc3b18125ce48007b30d8a77d6
Akmal Soliev <soliev@gmail.com>
2021-03-23 00:33:50 +0100

    Prepared all the files and git repo for addition. 23-03-2021 goal: Analyse the potential variable and comparatives that can be utilised for the research. Currently thinking of using weather patter to find correlation between increase in temperature and decrease in number of new cases.

--- Assignment4.py
+++ Assignment4.py
@@ -5,9 +5,19 @@ import matplotlib.pyplot as plt
 
 covid_data = pd.read_csv('Daily_Covis19_Italian_Data_Province_Incremental.csv')
 
-covid_data.plot()
+covid_positive = covid_data.groupby('Date').agg({'Total Positive': np.sum})
 
-covid_positive = covid_data.groupby('Date').agg(np.sum)
+fig, ax = plt.subplots(figsize=[8, 4.5])
 
-plt.plot(covid_positive)
+ax.plot(covid_positive)
+
+# Labeling
+plt.title('COVID-19 Italy \nNumber of new cases by from Feb 2019')
+plt.xlabel('Months')
+plt.ylabel('Number of people')
+
+# Setting x-tick labels.
+locations = np.arange(0, 393, 30)#393
+plt.xticks(locations, np.linspace(0, len(locations), len(locations), dtype=int))
+
 plt.show()
