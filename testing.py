dc417a6e083b825005b6b27024624c4b3c1060b6
Akmal Soliev <soliev@gmail.com>
2021-03-27 02:32:54 +0100

    Completed the second part The DataFrames are complete and ready to plotted. I'll construct two plots to showcase the information in seperate manner, followed by an aggregated plot.

--- testing.py
+++ testing.py
@@ -10,27 +10,54 @@ wiki_page = 'Climate_of_Italy'
 html = wp.page(wiki_page).html().replace(u'\u2212', '-')
 
 
-def dataframe_cleaning(table_number=int):
+def dataframe_cleaning(table_number: int):
     global html
     df = pd.read_html(html, encoding='utf-8')[table_number]
     df.drop(np.arange(5, len(df.index)), inplace=True)
     df.columns = df.columns.droplevel()
-    replace = '[Â°C(Â°F)]*'
-    df['Month'] = df['Month'].str.replace(replace, '', regex=True)
+    df.drop('Year', axis=1, inplace=True)
 
     find = '\((.*?)\)'
     for i, column in enumerate(df.columns):
         if i > 0:
             df[column] = (df[column]
                           .str.findall(find)
-                          .map(lambda x: np.round((float(x[0]) - 32) * (5 / 9), 2)))
+                          .map(lambda x: (float(x[0]) - 32) * (5 / 9)))
     return df
 
 
-test = dataframe_cleaning(1)
+# Setting up all the DataFrames that will be used to plot Italy's average temperature with high and low variation.
+
+potenza_df = dataframe_cleaning(3)
+milan_df = dataframe_cleaning(4)
+florence_df = dataframe_cleaning(6)
+
+# Constructing aggregated DataFrame for all temperatures in Italy:
+
+concat_df = pd.concat((potenza_df, milan_df, florence_df))
+
+italy_df = pd.DataFrame()
+for i, index in enumerate(list(set(concat_df['Month']))):
+    if i == 0:
+        temp_df = concat_df[concat_df['Month'] == index]
+        temp_df = temp_df.groupby('Month').agg(np.max)
+    if i in range(1, 4):
+        temp_df = concat_df[concat_df['Month'] == index]
+        temp_df = temp_df.groupby('Month').agg(np.mean)
+    if i == 4:
+        temp_df = concat_df[concat_df['Month'] == index]
+        temp_df = temp_df.groupby('Month').agg(np.min)
+    italy_df = italy_df.append(temp_df)
+
+italy_df = italy_df.apply(lambda x: np.round(x, 2))
+italy_df
 
 # Importing DATA
-covid_data = pd.read_csv('Daily_Covis19_Italian_Data_Province_Incremental.csv')
+url = 'https://github.com/DavideMagno/ItalianCovidData/blob/master/Daily_Covis19_Italian_Data_Province_Incremental.csv'
+covid_data = pd.read_csv(url, index_col=0)
+
+while True:
+    pass
 
 # Grouping
 covid_positive = covid_data.groupby('Date').agg({'Total Positive': np.sum})
